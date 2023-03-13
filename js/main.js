44bf270164e79d7b84aad1a0ef78a1510f7731c6
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-03-13 12:06:07 -0300

    Adicionado funcao sroll e secao sobre

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const menu = document.getElementById("cabecalho-menu");
 const navMenu = document.getElementById("cabecalho-navID");
+const cabecalho = document.getElementById("cabecalhoID");
 let verificadorMenu = 0;
 
 menu.addEventListener("click", () => {
@@ -12,4 +13,18 @@ menu.addEventListener("click", () => {
     menu.src = "img/menu.png";
     verificadorMenu = 0;
   }
+});
+
+const animarSroll = () => {
+  const windowTop = window.pageYOffset;
+  if (windowTop > 40) {
+    cabecalho.style.backgroundColor = "#141414";
+    cabecalho.style.transition = ".5s";
+  } else {
+    cabecalho.style.backgroundColor = "transparent";
+  }
+};
+
+window.addEventListener("scroll", () => {
+  animarSroll();
 });
