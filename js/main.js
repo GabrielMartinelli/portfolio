8fe790f8e77290a3964b080650c72ddc1a220414
GabrielMartinelli <gabrielmartinelli10@gmail.com>
2023-03-15 16:25:33 -0300

    Projetos adicionados

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById("cabecalho-menu");
 const navMenu = document.getElementById("cabecalho-navID");
 const cabecalho = document.getElementById("cabecalhoID");
+const slide = document.querySelectorAll("[data-slide]");
+
 let verificadorMenu = 0;
 
 menu.addEventListener("click", () => {
@@ -29,3 +31,25 @@ const animarSroll = () => {
 window.addEventListener("scroll", () => {
   animarSroll();
 });
+
+const teste = () => {
+  slide.forEach((elemento) => {
+    console.log(elemento);
+    elemento.addEventListener("mouseover", () => {
+      console.log(elemento.childNodes);
+      elemento.childNodes[5].classList.add("carrossel-divAcesso");
+      elemento.childNodes[1].classList.add("carrossel-divTitulo");
+      elemento.classList.remove("slide__fundo");
+    });
+  });
+
+  slide.forEach((elemento) => {
+    elemento.addEventListener("mouseleave", () => {
+      elemento.childNodes[5].classList.remove("carrossel-divAcesso");
+      elemento.childNodes[1].classList.remove("carrossel-divTitulo");
+      elemento.classList.add("slide__fundo");
+    });
+  });
+};
+
+teste();
